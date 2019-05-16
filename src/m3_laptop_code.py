50dd845887bbbaf8d81fb4823fbb86ff4798013c
millart <millart@rose-hulman.edu>
2019-05-16 15:46:21 -0400

    Added name

--- src/m3_laptop_code.py
+++ src/m3_laptop_code.py
@@ -21,11 +21,45 @@ def get_my_frame(root, window, mqtt_sender):
     frame = ttk.Frame(window, padding=10, borderwidth=5, relief="ridge")
     frame_label = ttk.Label(frame, text="Emily Millard")
     frame_label.grid()
+    speed_label = ttk.Label(frame, text="Speed")
+    speed_label.grid()
     # DONE 2: Put your name in the above.
 
     # Add the rest of your GUI to your frame:
     # TODO: Put your GUI onto your frame (using sub-frames if you wish).
+    speed_entry_box = ttk.Entry(frame, width=8)
+    speed_entry_box.insert(0, "100")
+    arm_calibrate_button = ttk.Button(frame, text="Calibrate Arm")
 
+    location_label = ttk.Label(frame, text="New Arm Location")
+    arm_location_entry_box = ttk.Entry(frame, width=8)
+    arm_location_entry_box.insert(0, "5")
+
+    arm_to_button = ttk.Button(frame, text="Arm to Location")
+    arm_up_button = ttk.Button(frame, text="Arm Up")
+    arm_down_button = ttk.Button(frame, text="Arm Down")
+
+    color_label = ttk.Label(frame, text="Color to Stop")
+    stop_color_entry_box = ttk.Entry(frame, width=8)
+    stop_color_entry_box.insert(0, "Red")
+    forward_until_color_button = ttk.Button(frame, text="Forward Until Color")
+
+    speed_entry_box.grid()
+    arm_calibrate_button.grid()
+    arm_calibrate_button['command'] = lambda: arm_calibration(speed_entry_box, mqtt_sender)
+    location_label.grid()
+    arm_location_entry_box.grid()
+    arm_to_button.grid()
+    arm_to_button['command'] = lambda: arm_to(speed_entry_box, arm_location_entry_box, mqtt_sender)
+    arm_up_button.grid()
+    arm_up_button['command'] = lambda: handle_arm_up(speed_entry_box, mqtt_sender)
+    arm_down_button.grid()
+    arm_down_button['command'] = lambda: handle_arm_down(speed_entry_box, mqtt_sender)
+    color_label.grid()
+    stop_color_entry_box.grid()
+    forward_until_color_button.grid()
+    forward_until_color_button['command'] = lambda: \
+        forward_until_color(speed_entry_box, stop_color_entry_box, mqtt_sender)
     # Return your frame:
     return frame
 
@@ -47,3 +81,33 @@ class MyLaptopDelegate(object):
 
 
 # TODO: Add functions here as needed.
+def handle_arm_up(speed_entry_box, mqtt_sender):
+    print("handle arm up:", speed_entry_box.get())
+    speed = int(speed_entry_box.get())
+    mqtt_sender.send_message("arm_up", [speed])
+
+
+def handle_arm_down(speed_entry_box, mqtt_sender):
+    print("handle arm down:", speed_entry_box.get())
+    speed = int(speed_entry_box.get())
+    mqtt_sender.send_message("arm_down", [speed])
+
+
+def arm_calibration(speed_entry_box, mqtt_sender):
+    print("Calibrating")
+    speed = int(speed_entry_box.get())
+    mqtt_sender.send_message("arm_calibrate", [speed])
+
+
+def arm_to(speed_entry_box, arm_location_entry_box, mqtt_sender):
+    print("moving arm to:", arm_location_entry_box.get())
+    speed = int(speed_entry_box.get())
+    location = int(arm_location_entry_box.get())
+    mqtt_sender.send_message("arm_to", [speed, location])
+
+
+def forward_until_color(speed_entry_box, stop_color_entry_box, mqtt_sender):
+    print("forward until detection of color:", stop_color_entry_box.get())
+    speed = int(speed_entry_box.get())
+    color = str(stop_color_entry_box.get())
+    mqtt_sender.send_message("forward_to_color", [speed, color])
--- src/m3_robot_code.py
+++ src/m3_robot_code.py
@@ -1,10 +1,10 @@
 """
   Capstone Project.  Code to run on the EV3 robot (NOT on a laptop).
   Author:  Your professors (for the framework)
-    and PUT_YOUR_NAME_HERE.
+    and Emily Millard.
   Spring term, 2018-2019.
 """
-# TODO 1:  Put your name in the above.
+# DONE 1:  Put your name in the above.
 
 import mqtt_remote_method_calls as mqtt
 import rosebot
@@ -22,11 +22,62 @@ class MyRobotDelegate(object):
         self.robot = robot  # type: rosebot.RoseBot
         self.mqtt_sender = None  # type: mqtt.MqttClient
         self.is_time_to_quit = False  # Set this to True to exit the robot code
+        self.robot.sensor_system.color_sensor.get_color()
 
     def set_mqtt_sender(self, mqtt_sender):
         self.mqtt_sender = mqtt_sender
 
     # TODO: Add methods here as needed.
+    def arm_up(self, speed):
+        """ Moves the arm all the way to its touch sensor """
+        print(speed)
+        self.robot.arm_and_claw.motor.turn_on(speed)
+        while True:
+            if self.robot.arm_and_claw.touch_sensor.is_pressed():
+                self.robot.arm_and_claw.motor.turn_off()
+                break
+
+    def arm_calibrate(self, speed):
+        self.arm_up(speed)
+        self.robot.arm_and_claw.motor.reset_position()
+        self.robot.arm_and_claw.motor.turn_on(-speed)
+        while True:
+            print(self.robot.arm_and_claw.motor.get_position())
+            if self.robot.arm_and_claw.motor.get_position() <= -14.2 * 360:
+                self.robot.arm_and_claw.motor.reset_position()
+                self.robot.arm_and_claw.motor.turn_off()
+                break
+
+    def arm_to(self, speed, location):
+        """ Moves the arm to the position given """
+        current_position = self.robot.arm_and_claw.motor.get_position()
+        print_message_received("arm_to", current_position)
+        move = location - current_position
+        if move > 0:
+            self.robot.arm_and_claw.motor.turn_on(speed)
+            while True:
+                if self.robot.arm_and_claw.motor.get_position() >= location:
+                    self.robot.arm_and_claw.motor.turn_off()
+                    break
+        elif move < 0:
+            self.robot.arm_and_claw.motor.turn_on(-speed)
+            while True:
+                if self.robot.arm_and_claw.motor.get_position() <= location:
+                    self.robot.arm_and_claw.motor.turn_off()
+                    break
+
+    def arm_down(self, speed):
+        """ Moves the arm all the way to position zero """
+        self.arm_to(speed, 0)
+
+    def forward_to_color(self, speed, color):
+        color_number = self.robot.sensor_system.color_sensor.get_color_number_from_color_name(color)
+        print(color_number, color)
+        while True:
+            self.robot.drive_system.go(speed, speed)
+            if self.robot.drive_system.sensor_system.color_sensor.get_color() == color_number:
+                self.robot.drive_system.stop()
+                break
 
 
 def print_message_received(method_name, arguments=None):
